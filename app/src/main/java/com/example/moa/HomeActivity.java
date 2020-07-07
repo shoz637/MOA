76acfa6a529243e0fd6aa852c7d112e7a48c0a22
yebin kang <21800007@handong.edu>
2020-07-07 19:49:18 +0900

    Initial commit

---
+++ app/src/main/java/com/example/moa/HomeActivity.java
@@ -0,0 +1,37 @@
+package com.example.moa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class HomeActivity extends AppCompatActivity {
+    static final String[] LIST_MENU = {"LIST1", "LIST2", "LIST3"} ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, LIST_MENU) ;
+
+        ListView listview = (ListView) findViewById(R.id.listview1) ;
+        listview.setAdapter(adapter) ;
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            // 코드 계속 ...
+
+            @Override
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+
+                // get TextView's Text.
+                String strText = (String) parent.getItemAtPosition(position) ;
+
+                // TODO : use strText
+            }
+        }) ;
+    }
+}
+
---
+++ app/src/main/java/com/example/moa/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.moa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+
+    public void sign(View view) {
+        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+        startActivity(intent);
+    }
+
+}
